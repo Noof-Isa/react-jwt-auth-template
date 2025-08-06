0166b82a1656228c87c84f8b9c7eacd4318b8680
Noof-Isa <noofisa786@gmail.com>
2025-08-06 14:59:37 +0300

    confrim the sign-up password

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -8,6 +8,8 @@ const navigate = useNavigate()
 const initialState = {
     username: '',
     password: '',
+        passwordConf: '',
+
   }
 
   const [formData, setFormData] = useState(initialState)
@@ -23,7 +25,11 @@ const initialState = {
         navigate('/')
 
   }
+  let formIsInvalid = true
 
+  if (formData.username && formData.password && formData.password === formData.passwordConf) {
+    formIsInvalid = false
+  }
   return (
     <main>
       <h1>Sign up Form</h1>
@@ -34,7 +40,12 @@ const initialState = {
         <label>Password:</label>
         <input type="password" name='password' onChange={handleChange} />
         <br />
-        <button type="submit">Sign up</button>
+                <label>Confirm Password:</label>
+        <input type="password" name="passwordConf" onChange={handleChange} />
+        <br />
+        <button type="submit" disabled={formIsInvalid}>Sign up</button>
+
+      
       </form>
     </main>
   )
